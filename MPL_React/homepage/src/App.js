1435f060ad8fba2faced7713875c24ecc46eff30
Felipe Vinicius de Sousa <31699547+benzerinsio@users.noreply.github.com>
2023-06-06 20:55:31 -0300

    Evolving the navbar component

--- MPL_React/homepage/src/App.js
+++ MPL_React/homepage/src/App.js
@@ -5,12 +5,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
         <h1>Bem-vindo à Homepage</h1>
         <p>Esta é a página inicial do meu site.</p>
       </header>
       <div>
-      <Navbar />
       <h1>Conteúdo da Págine</h1>
     </div>
     </div>
