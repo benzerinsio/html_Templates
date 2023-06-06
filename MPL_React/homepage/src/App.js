69072710f0c7793532b9d7af6c4a1d5539d48af9
Felipe Vinicius de Sousa <31699547+benzerinsio@users.noreply.github.com>
2023-06-05 23:19:33 -0300

    Creating first project in react

---
+++ MPL_React/homepage/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Bem-vindo à Homepage</h1>
+        <p>Esta é a página inicial do meu site.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
