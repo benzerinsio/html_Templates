757aef163b13196de2cfca5924e399b6264bdd2d
Felipe Vinicius de Sousa <31699547+benzerinsio@users.noreply.github.com>
2023-06-30 20:39:50 -0300

    Creating a new project
    Creating a tic-tac-toe by following the tutorial on react's site

---
+++ react/ljg0t8/src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+function Square(props) {
+  function clickHandler() {
+    console.log("Clicked!!");
+  }
+
+  return (
+    <button className="square" onClick={clickHandler}>
+      {props.value}
+    </button>
+  );
+}
+
+export default function Board() {
+  return (
+    <div>
+      <div className="board-row">
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
+      </div>
+      <div className="board-row">
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
+      </div>
+      <div className="board-row">
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
+      </div>
+    </div>
+  );
+}
