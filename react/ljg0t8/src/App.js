ab2ccaf950ba4cbf6e9915ddbbeda317a8c01d62
Felipe Vinicius de Sousa <31699547+benzerinsio@users.noreply.github.com>
2023-07-01 01:23:04 -0300

    Continuing the tutorial

--- react/ljg0t8/src/App.js
+++ react/ljg0t8/src/App.js
@@ -1,34 +1,39 @@
 import React from "react";
+import { useState } from "react";
 
-function Square(props) {
-  function clickHandler() {
-    console.log("Clicked!!");
-  }
-
+function Square(props, onSquareClick) {
   return (
-    <button className="square" onClick={clickHandler}>
-      {props.value}
+    <button className="square" onClick={onSquareClick}>
+      {/*{value*/}
     </button>
   );
 }
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function clickHandler() {
+    const nextSquares = squares.slice();
+    nextSquares[0] = "X";
+    setSquares(nextSquares);
+  }
+
   return (
     <div>
       <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+        <Square value={squares[0]} onSquareClick={clickHandler} />
+        <Square value={squares[1]} onSquareClick={clickHandler} />
+        <Square value={squares[2]} onSquareClick={clickHandler} />
       </div>
       <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+        <Square value={squares[3]} onSquareClick={clickHandler} />
+        <Square value={squares[4]} onSquareClick={clickHandler} />
+        <Square value={squares[5]} onSquareClick={clickHandler} />
       </div>
       <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+        <Square value={squares[6]} onSquareClick={clickHandler} />
+        <Square value={squares[7]} onSquareClick={clickHandler} />
+        <Square value={squares[8]} onSquareClick={clickHandler} />
       </div>
     </div>
   );
